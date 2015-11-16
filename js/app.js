bbdc0611216ea91e53d5780a3ada06d06e6d83b8
Ellert Smári <ellertsmari@fss.is>
2015-11-16 12:53:55 +0000

    merged Jonas

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
-var app = angular.module('MyndaApp', ['ngRoute']);
-app.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      controller: "MainController",
-      templateUrl: "views/homepage.html"
-    })
-    .when('LINKFYRIRLOGINDOITLATERYOUSEXYBABES', {
-      controller: 'LoginController',
-      templateUrl: 'views/login.html'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
-});
+var app = angular.module('MyndaApp', ['ngRoute']).config(['$routeProvider','$locationProvider',
+  function($routeProvider, $locationProvider) {
+      $routeProvider
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginController'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'RegisterController'
+      })
+      .when('/', {
+        controller: "MainController",
+        templateUrl: "views/homepage.html"
+      })
+      .when('LINKFYRIRLOGINDOITLATERYOUSEXYBABES', {
+        controller: 'LoginController',
+        templateUrl: 'views/login.html'
+      })
+      .otherwise({
+        redirectTo: '/login'
+      });
+  }]);
