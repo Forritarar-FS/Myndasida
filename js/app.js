0b04756dd3ae1ee721a2b121ddb480193b9017ac
Ellert Smári <ellertsmari@fss.is>
2015-11-17 12:25:50 +0000

    merged jonas

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
+<<<<<<< HEAD
 var app = angular.module('MyndaApp', ['ngRoute']).config(['$routeProvider','$locationProvider',
   function($routeProvider, $locationProvider) {
       $routeProvider
       .when('/login', {
         templateUrl: 'views/login.html',
         controller: 'LoginController'
-      })
-      .when('/register', {
-        templateUrl: 'views/register.html',
-        controller: 'RegisterController'
       })
       .when('/', {
         controller: "MainController",
@@ -16,8 +13,19 @@ var app = angular.module('MyndaApp', ['ngRoute']).config(['$routeProvider','$loc
       .when('LINKFYRIRLOGINDOITLATERYOUSEXYBABES', {
         controller: 'LoginController',
         templateUrl: 'views/login.html'
+      .when('/signup', {
+        templateUrl: 'views/register.html',
+        controller: 'RegisterController'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'RegisterController'
       })
       .otherwise({
         redirectTo: '/login'
       });
   }]);
+<<<<<<< HEAD
+=======
+  console.log("hello")
+>>>>>>> aa2ac3a34038a3cc6a7afa75026d8ed9111c3aba
