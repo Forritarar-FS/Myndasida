4aad81418049e7b410e053117adf461e38b0dcf6
Ellert Smári <ellertsmari@fss.is>
2015-12-11 14:11:06 +0000

    merged from jonas

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -1,8 +1,5 @@
 app.controller('LoginController', ['$scope', function($scope) {
   $scope.greeting = 'Hola!';
   document.body.style.background = "url(http://i.imgur.com/c9HtRon.jpg)";
-
-    $scope.Submit = function(){
-      alert("hallo");
-    }
+  
 }]);
